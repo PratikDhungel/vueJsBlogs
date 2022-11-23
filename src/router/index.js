699ae1a70cf9a30f280babc10eaa34d1795de46a
Pratik Dhungel <pratikdhungel7.0@gmail.com>
2022-11-23 12:05:34 +0545

    Vue JS Blogs: Update page title for Home and Blogs view

--- src/router/index.js
+++ src/router/index.js
@@ -6,17 +6,28 @@ const routes = [
     path: '/',
     name: 'HomeView',
     component: HomeView,
+    meta: {
+      title: 'Home',
+    },
   },
   {
     path: '/blogs',
     name: 'BlogsView',
     component: BlogsView,
+    meta: {
+      title: 'Blogs',
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, _, next) => {
+  document.title = `${to.meta.title} | VueJS Blogs`;
+  next();
 });
 
 export default router;
