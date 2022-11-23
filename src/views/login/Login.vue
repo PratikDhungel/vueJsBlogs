6970af86c703f2020befaa615f69661d6fb03735
Pratik Dhungel <pratikdhungel7.0@gmail.com>
2022-11-23 15:51:34 +0545

    Vue JS Blogs: Create login form and update styling

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,3 +1,40 @@
 <template>
-  <div>Login</div>
+  <div class="form-wrap">
+    <form class="login">
+      <p class="login-register">
+        Don't have an account?
+        <router-link class="router-link" :to="{ name: 'RegisterView' }"
+          >Register</router-link
+        >
+      </p>
+
+      <h2>Login to VueJS Blogs</h2>
+
+      <div class="inputs">
+        <div class="input">
+          <input type="text" name="email" v-model="email" />
+          <img :src="emailIcon" alt="email-icon" class="icon" />
+        </div>
+
+        <div class="input">
+          <input type="text" name="password" v-model="password" />
+          <img :src="passwordIcon" alt="password-icon" class="icon" />
+        </div>
+      </div>
+
+      <router-link class="router-link" :to="{ name: 'ForgotPasswordView' }">
+        Forgot your password?</router-link
+      >
+
+      <button>Sign In</button>
+
+      <div class="angle"></div>
+    </form>
+
+    <div class="background"></div>
+  </div>
 </template>
+
+<style lang="scss" src="./login.style.scss" />
+
+<script src="./login.view.js" />
--- src/views/login/login.view.js
+++ src/views/login/login.view.js
@@ -1,3 +1,9 @@
+import emailIcon from '../../assets/Icons/envelope-regular.svg';
+import passwordIcon from '../../assets/Icons/lock-alt-solid.svg';
+
 export default {
   name: 'LoginView',
+  data() {
+    return { email: '', password: '', emailIcon, passwordIcon };
+  },
 };
